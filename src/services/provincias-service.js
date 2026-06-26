f6766448f512f404903963872eb0026108ca76e3
Luca Gamas <lucagamasdemaio@gmail.com>
2026-06-26 11:53:13 -0300

    added reglas de negocio

--- src/services/provincias-service.js
+++ src/services/provincias-service.js
@@ -19,12 +19,18 @@ export default class ProvinciasService {
     }
 
     createAsync = async (entity) => {
+        if(entity == null){throw new Error("Ninguna data fue enviada. ERRORRRRRR :P")}
+        validateFields(["name", "full_name", "latitude", "longitude", "display_order"], entity)
+        if(entity.name.length < 3){throw new Error("El nombre debe medir 3 letras por lo menos")}
         
         const rowsAffected = await this.CurrentRepository.createAsync(entity);
         return rowsAffected;
     }
 
     updateAsync = async (entity) => {
+        if(entity == null){throw new Error("Ninguna data fue enviada. ERRORRRRRR :P")}
+        if(entity.name != undefined && entity.name.length < 3){throw new Error("El nombre debe medir 3 letras por lo menos")}
+
         const rowsAffected = await this.CurrentRepository.updateAsync(entity);
         return rowsAffected;
     }
@@ -35,4 +41,16 @@ export default class ProvinciasService {
     }
  
  
+}
+
+
+const validateFields = (requiredFields, object) => {
+    
+    for (const field of requiredFields) {
+        if (!object[field]) {
+            throw new Error(`\"${field}\" no fue incluido en la peticion :S`);
+        }
+    }
+
+    return true
 }
